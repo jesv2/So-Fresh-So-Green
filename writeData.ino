3c7a88473c3a90503b1da5d9a59a1927e4c5a478
drodrgz5 <36084003+drodrgz5@users.noreply.github.com>
2020-03-13 15:30:28 -0500

    writeData now works

--- writeData.ino
+++ writeData.ino
@@ -12,9 +12,12 @@
 File myFile;
 const int pinCS = 53;
 
+///////////////////////////////////////////
+// Used to test WriteData function
 const int n = 10; 
 const String testName = "datatest";
 int testData[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+///////////////////////////////////////
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -29,16 +32,25 @@ void setup() {
   }
 Serial.println("initialization done.");
 
-  bool success = WriteData(testData, testName, n, false); 
+  bool success = WriteData(testData, testName, n, false); //Tests writeData function 
 }
-
+/*
+ * WriteData() Inputs/Outputs
+ * Inputs: data, name of file, number of data points, whether it's a new file 
+ * Outputs: Writes data to CSV (excel) file which will be located on microSD
+ *      column1 | column2 | column3 | etc.    
+ *      datapt1 | datapt2 | datapt3 | etc.
+ * 
+ */
 bool WriteData( int inputData[], String fileName, int count, bool newFile ){ //didn't use newFile
   //same stuff inside 
     myFile = SD.open( fileName + ".csv", FILE_WRITE); //open csv file to write in
-    if(myFile){                                         //checks to see if opened correctly
+    if(myFile){                                       //checks to see if opened correctly
       for (int i = 0; i < count; i++ ) {
-       myFile.print( String(inputData[i]) + ',' ); //might end up adding ascii value of comma
-       Serial.write( inputData[i] + ',' ); //writes same val. to serial monitor (use for debugging)
+       myFile.print( String(inputData[i]));           //might end up adding ascii value of comma
+       myFile.print( ',' );                           //separates values
+       Serial.print( inputData[i]);                   //writes same val. to serial monitor (use for debugging)
+       Serial.print( ',' );
       }
       myFile.print('\n');
       myFile.close();
@@ -49,6 +61,7 @@ bool WriteData( int inputData[], String fileName, int count, bool newFile ){ //d
       return false;
       }
 }
-void loop() { //need to include this or else error w/ multiple libraries will occur
+// We should add a function that deletes the old files or overwrites the old data
+void loop() {                                       //need to include this or else error w/ multiple libraries will occur
   // nothing happens after setup
 }
