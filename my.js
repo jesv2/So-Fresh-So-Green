bdba429f2303a7dae61368d386d95e94d9edbe6b
flyingpiggs <chen172@uic.edu>
2020-03-21 20:23:31 -0500

    Added draggable divs to the content tabs

--- my.js
+++ my.js
@@ -1,3 +1,4 @@
+
 function setupTabs () {
     document.querySelectorAll(".tabs__button").forEach(button => {
         button.addEventListener("click", () => {
@@ -20,11 +21,86 @@ function setupTabs () {
     });
 }
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    setupTabs();
+    setupTabs(); 
 
     //Displays the first tabs content by default
     document.querySelectorAll(".tabs").forEach(tabsContainer => {
         tabsContainer.querySelector(".tabs__sidebar .tabs__button").click();
     });
 });
+
+/* This block is code is for making the divs that we can drag around to indicate what the state of each pin is */
+const pinCount = 10;
+const boxName = 'notUsedPins'; 
+
+MakePinDivs();
+
+function MakePinDivs() {
+  let boxForPins = document.getElementById( boxName ); 
+  for( let counter = 2; counter <= pinCount; counter++ ) {
+    let newDiv = document.createElement('div');
+    newDiv.className = 'draggable';
+    newDiv.draggable = 'true';
+    newDiv.ondragstart = "event.dataTransfer.setData('text/plain',null)";
+    newDiv.innerHTML = 'pin' + counter.toString();
+    boxForPins.append( newDiv ); 
+  }
+} 
+
+//Source: https://developer.mozilla.org/en-US/docs/Web/API/Document/dragstart_event
+
+let dragged;
+
+  /* events fired on the draggable target */
+  document.addEventListener("drag", function( event ) {
+
+  }, false);
+
+  document.addEventListener("dragstart", function( event ) {
+      // store a ref. on the dragged elem
+      dragged = event.target;
+      // make it half transparent
+      event.target.style.opacity = .5;
+  }, false);
+
+  document.addEventListener("dragend", function( event ) {
+      // reset the transparency
+      event.target.style.opacity = "";
+  }, false);
+
+  /* events fired on the drop targets */
+  document.addEventListener("dragover", function( event ) {
+      // prevent default to allow drop
+      event.preventDefault();
+  }, false);
+
+  document.addEventListener("dragenter", function( event ) {
+      // highlight potential drop target when the draggable element enters it
+      if ( event.target.className == "dropzone" ) {
+          event.target.style.background = "purple";
+      }
+
+  }, false);
+
+  document.addEventListener("dragleave", function( event ) {
+      // reset background of potential drop target when the draggable element leaves it
+      if ( event.target.className == "dropzone" ) {
+          event.target.style.background = "";
+      }
+
+  }, false);
+
+  document.addEventListener("drop", function( event ) {
+      // prevent default action (open as link for some elements)
+      event.preventDefault();
+      // move dragged elem to the selected drop target
+      if ( event.target.className == "dropzone" ) {
+          event.target.style.background = "";
+          dragged.parentNode.removeChild( dragged );
+          event.target.appendChild( dragged );
+      }
+    
+  }, false);
